7861d131c03dec3ac7b56df2afc9cfaecc0635e9
nocturnalking <harsh.nocturnalking@gmail.com>
2020-02-10 23:35:14 -0600

    Big O N find

--- Basic1/Basic1/Main.cpp
+++ Basic1/Basic1/Main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+string Name[] = {"Nemo"};
+
+
+
+void FindNemo(string arry[])
+{
+	for (size_t i = 0; i < arry->length(); i++)
+	{
+		if (arry[i] == "Nemo")
+		{
+			printf("Found Nemo!!");
+		}
+	}
+}
 
 int main()
 {
-	printf("Hello World");
+	FindNemo(Name);
 }
